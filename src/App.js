c74e6a60fca94a1939fc8288b426d08f14fda3d9
gennaro maida <gtm1235@gmail.com>
2023-10-11 10:11:54 -0500

    FIrst commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React from "react";
+import CardList from "./CardList";
+//import { robots } from "./robots";
+import SearchBox from './SearchBox';
+import Scroll from './Scroll';
+import './App.css'
+
+
+
+class App extends React.Component {
+    constructor() {
+        //super is required with constructor with extends
+        super()
+        this.state = {
+            robots: [],
+            searchfield: ''
+        }
+    }
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users })
+            )
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+        //This is self built component so needs arrow function so this.state.robots works
+
+
+        //console.log(filteredRobots)
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        })
+        if (this.state.robots.length === 0) {
+            return <h1>Loading...</h1>
+        } else {
+
+            return (
+                <div className='tc' >
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <Scroll>
+                <CardList robots={filteredRobots} />
+                </Scroll>
+                </div>
+            )
+        }
+    }
+}
+
+export default App
---
+++ src/CardList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Card from './Card';
+//from
+
+const CardList = ({ robots }) => {
+
+    return (
+        <div>
+            {
+                robots.map((user, index) => {
+                    return (
+                        <Card
+                            key={robots[index].id}
+                            id={robots[index].id}
+                            name={robots[index].name}
+                            email={robots[index].email}
+                        />
+                    );
+                })
+            }
+        </div>
+    );
+}
+
+export default CardList
