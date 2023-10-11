1440e975ab1ff0a33a5530f7c95f6dc2743c1e1d
gennaro maida <gtm1235@gmail.com>
2023-10-11 10:51:59 -0500

    Clean up code

--- src/containers/App.js
+++ src/containers/App.js
@@ -29,14 +29,13 @@ class App extends React.Component {
     }
 
     render() {
-        const filteredRobots = this.state.robots.filter(robots => {
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
         })
-        if (this.state.robots.length === 0) {
-            return <h1>Loading...</h1>
-        } else {
-
-            return (
+        return !robots.length ?
+            <h1>Loading...</h1> :
+            (
                 <div className='tc' >
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
@@ -45,7 +44,6 @@ class App extends React.Component {
                     </Scroll>
                 </div>
             )
-        }
     }
 }
 
